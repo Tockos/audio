d06d7b230d7421970b119959847796a1e63ebb23
Péter Tóth <petertoth11@gmail.com>
2020-11-03 22:12:28 +0100

    patches needed

---
+++ flute_spectra.py
@@ -0,0 +1,22 @@
+import wave
+import numpy as np
+import sounddevice as sd
+import matplotlib.pyplot as plt
+
+orig_flute = wave.open("examples/flute.wav", "rb")
+flute_params = orig_flute.getparams()
+fs = orig_flute.getframerate()
+frames = orig_flute.readframes(orig_flute.getnframes())
+dt = np.int16
+samples = np.frombuffer(frames, dt)
+samples32 = samples.astype(np.float32)
+max_amp = 2**15
+norm_samples = samples32/max_amp
+
+# sd.play(norm_samples, 2*fs)
+# status = sd.wait()  # Wait until file is done playing
+a = 10
+flute_spect = np.fft.rfft(norm_samples)
+plt.plot(flute_spect)
+# plt.plot(norm_samples[:100000])
+plt.show()
